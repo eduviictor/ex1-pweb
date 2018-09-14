76c9b5dd2118a69bc6e7c04cd7597e7c22335732
eduviictor <eduvictornobrega@gmail.com>
2018-09-14 14:30:10 -0300

    Adicionando 2 sections (formação e conhecimentos) e começando a section contato

---
+++ src/main/java/br/ufrn/ceres/bsi/pweb/ex1/servlet/Controller.java
@@ -0,0 +1,52 @@
+package br.ufrn.ceres.bsi.pweb.ex1.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    
+	    String email = request.getParameter("email");
+	    String senha = request.getParameter("senha");
+	    if(email.equals("edu@gmail.com") && senha.equals("12345")) {
+	        request.getRequestDispatcher("q4.html").forward(request, response);
+	    }else {
+	        request.getRequestDispatcher("q6.html").forward(request, response);
+	    }
+	}
+
+}
